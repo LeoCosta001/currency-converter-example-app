77945b355d90032ada9c1c299540d091f6230f58
LeoCosta <leocosta.black@hotmail.com>
2022-12-04 18:10:59 -0300

    Create home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,31 @@
+import 'package:currency_converter/pages/home_page.dart';
+import 'package:flutter/material.dart';
 
+const Color mainColor = Color(0xFFEDA800);
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: mainColor,
+        ),
+        iconTheme: const IconThemeData(color: mainColor),
+        textTheme: const TextTheme(
+          button: TextStyle(color: Colors.white),
+          bodyText1: TextStyle(color: Colors.white),
+          bodyText2: TextStyle(color: Colors.white),
+        ),
+      ),
+      home: const Home(),
+    );
+  }
+}
