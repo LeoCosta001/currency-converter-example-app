fb8b3fb8515cdf1f6e1bc535020d81cf0f33d644
LeoCosta <leocosta.black@hotmail.com>
2022-12-04 21:09:23 -0300

    Create secret file

---
+++ lib/auth/secrets.dart
@@ -0,0 +1,2 @@
+// Get API key in "https://console.hgbrasil.com/documentation/finance"
+const String hgFinanceApiKey = '<API_KEY>';
--- lib/http/hg_finance_api/hg_finance_api_webclient.dart
+++ lib/http/hg_finance_api/hg_finance_api_webclient.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
 
 import 'package:http/http.dart';
+import 'package:currency_converter/auth/secrets.dart';
 
 final Client client = Client();
-const String apiKey = '???';
 
 class HgFinanceApiWebClient {
   Future<Map<String, dynamic>> getHgFinanceApi() async {
     final Response response = await client.get(
-      Uri.parse('https://api.hgbrasil.com/finance?key=$apiKey'),
+      Uri.parse('https://api.hgbrasil.com/finance?key=$hgFinanceApiKey'),
     ); // Get API response JSON in string
 
     final Map<String, dynamic> responseBodyToJson = jsonDecode(response.body); // Convert string to JSON format
